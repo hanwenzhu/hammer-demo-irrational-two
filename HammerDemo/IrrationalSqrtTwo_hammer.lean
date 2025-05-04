04a2d01eea967d1fa41fc2bffcd3dc01f0ff6482
Thomas Zhu <thomas.zhu.sh@hotmail.com>
2025-05-04 17:53:49 -0600

    update hammer url

--- HammerDemo/IrrationalSqrtTwo_hammer.lean
+++ HammerDemo/IrrationalSqrtTwo_hammer.lean
@@ -8,9 +8,7 @@ register_hammer_extension linarith
 namespace Lemma
 
 theorem two_dvd_of_two_dvd_sq {m : ℕ} (h : 2 ∣ m ^ 2) : 2 ∣ m := by
-  -- `hammer`able
-  rw [pow_two, Nat.prime_two.dvd_mul] at h
-  cases h <;> assumption
+  hammer
 
 end Lemma
 
@@ -21,29 +19,19 @@ theorem irrational_sqrt_two {m n : ℕ} (coprime_mn : m.Coprime n) :
     m ^ 2 ≠ 2 * n ^ 2 := by
   intro sqr_eq
   have : 2 ∣ m := by
-    -- `hammer`able
-    apply Lemma.two_dvd_of_two_dvd_sq
-    use n ^ 2
+    hammer
   obtain ⟨k, meq⟩ := dvd_iff_exists_eq_mul_left.mp this
   have : 2 * (2 * k ^ 2) = 2 * n ^ 2 := by
-    rw [← sqr_eq, meq]
-    ring
+    hammer
   have : 2 * k ^ 2 = n ^ 2 := by
-    -- `hammer`able
-    exact mul_left_cancel₀ two_ne_zero this
+    hammer
   have : 2 ∣ n := by
-    -- `hammer`able
-    apply Lemma.two_dvd_of_two_dvd_sq
-    use k ^ 2
-    exact this.symm
+    hammer
   have : 2 ∣ m.gcd n := by
-    -- `hammer`able after `clear coprime_mn`
-    rw [Nat.dvd_gcd_iff]
-    constructor <;> assumption
+    clear coprime_mn
+    hammer
   have : 2 ∣ 1 := by
-    -- `hammer`able
-    rwa [coprime_mn.gcd_eq_one] at this
-  -- `hammer`able
-  norm_num at this
+    hammer
+  hammer
 
 end IrrationalSqrtTwo
